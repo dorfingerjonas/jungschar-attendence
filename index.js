af20233a05588df39313c92192b57ed0ec35c9e0
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-09-28 19:50:32 +0200

    handle login validation

--- index.js
+++ index.js
@@ -6,20 +6,18 @@ const schedule = require('node-schedule');
 const fs = require('fs');
 
 const RequestHandler = require('./controller/RequestHandler');
-const ChildrenRepository = require('./controller/ChildrenRepository');
-const TutorRepository = require('./controller/TutorRepository');
-const childrenRepo = new ChildrenRepository();
-const tutorRepo = new TutorRepository();
 const reqHandler = new RequestHandler();
 
 app.use(express.static('public'));
 app.use(express.static('public/font'));
 app.use(express.static('public/icons'));
 app.use(express.static('public/style'));
-app.use(express.static('public/scripts'));
+app.use(express.static('public/script'));
 
 io.on('connection', socket => {
-    console.log('user connected');
+    socket.on('login data', async data => {
+        socket.emit('login validation', await reqHandler.validateCredentials(data));
+    });
 });
 
 http.listen(3000, () => {

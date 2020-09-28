bf58d086e62686391cb2b757462ded751fd6c0a4
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-09-28 19:50:20 +0200

    add validateCredentials(9 function reqHandler

--- controller/RequestHandler.js
+++ controller/RequestHandler.js
@@ -2,9 +2,15 @@ const fs = require('fs');
 const { promisify } = require('util');
 const ChildrenRepository = require('./ChildrenRepository');
 const TutorRepository = require('./TutorRepository');
+const UserRepository = require('./UserRepository');
+const childrenRepo = new ChildrenRepository();
+const tutorRepo = new TutorRepository();
+const userRepo = new UserRepository();
 
 class RequestHandler {
-
+    async validateCredentials(data) {
+        return await userRepo.validateCredentials(data);
+    }
 }
 
 module.exports = RequestHandler;

608b33c6b178430fdcbb6d4531ce55724a69fa57
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-09-29 21:26:10 +0200

    implement getUrlParams() function

--- public/script/main.js
+++ public/script/main.js
@@ -7,3 +7,26 @@ function getSocket() {
 
     return socket;
 }
+
+function getUrlParams(url) {
+    if (url !== undefined && url !== null && url.trim() !== '') {
+        const params = [];
+        const parts = url.split('?');
+
+        for (let i = 1; i < parts.length; i++) {
+            if (parts[i].includes('&')) {
+                const subPart = parts[i].split('&');
+
+                for (const part of subPart) {
+                    const splitted = part.split('=');
+                    params[`${splitted[0]}`] = splitted[1];
+                }
+            } else {
+                const subPart = parts[i].split('=');
+                params[`${subPart[0]}`] = subPart[1];
+            }
+        }
+
+        return params;
+    }
+}

bdeb1976117dbe91bc1857d2c69d371468808da8
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-09-29 21:18:20 +0200

    implement GroupsRepository.js

---
+++ controller/GroupsRepository.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+class GroupsRepository {
+    async add(group) {
+        const currentFile = await this.getAll();
+
+        group.groupId = Date.now();
+
+        currentFile.push(group);
+
+        fs.writeFile('./data/groups.json', JSON.stringify(currentFile), err => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+    async update(group) {
+        const groups = await this.getAll();
+
+        groups[groups.findIndex(r => r.groupId === group.groupId)] = group;
+
+        fs.writeFile('./data/groups.json', JSON.stringify(groups), err => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+    async delete(group) {
+        const currentFile = await this.getAll();
+
+        fs.writeFile('./data/groups.json', JSON.stringify(currentFile.filter(r => r.groupId !== group.groupId)), err => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+    deleteAll() {
+        fs.writeFile('./data/groups.json', JSON.stringify([]), err => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+    async getAll() {
+        return JSON.parse(await promisify(fs.readFile)('./data/groups.json', 'utf8'));
+    }
+}
+
+module.exports = GroupsRepository;

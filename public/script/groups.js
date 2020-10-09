10ed5205dd5b8585def090224c44eff8248e14bc
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-10-09 16:46:21 +0200

    print createBackup Btn

--- public/script/groups.js
+++ public/script/groups.js
@@ -38,17 +38,37 @@ window.addEventListener('load', () => {
 
         const buttonWrapper = document.createElement('div');
         const manageBtn = document.createElement('div');
+        const createBtn = document.createElement('div');
 
         manageBtn.textContent = 'Inhalte verwalten';
+        createBtn.textContent = 'E-Mail senden';
 
         manageBtn.addEventListener('click', () => {
             window.location.href = `${window.location.origin}/edit`;
         });
+        
+        createBtn.addEventListener('click', () => {
+            if (confirm('E-Mail wirklich senden?')) {
+                console.log('hellp');
+                socket.emit('cmd:create-backup', null);
+            }
+        });
+
+        socket.on('res:create-backup', success => {
+            console.log(success);
+            if (success) {
+                alert('E-Mail erfolgreich versendet');
+            } else {
+                alert('Senden fehlgeschlagen');
+            }
+        });
 
         buttonWrapper.classList.add('buttonWrapper');
         manageBtn.classList.add('button');
+        createBtn.classList.add('button');
 
         buttonWrapper.appendChild(manageBtn);
+        buttonWrapper.appendChild(createBtn);
         parent.appendChild(buttonWrapper);
     });
 });

e128cd39fc1bdb6621c7d4932e97e34dcdd984a7
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-30 20:19:46 +0200

    add endpoints array and add /children endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const endpoints = {
+    childrenEP: require('./services/ChildrenEndpoint')
+};
+
 const config = {
     port: 8080
 }
@@ -9,6 +13,8 @@ app.get('/', (req, res) => {
     res.contentType('application/json');
     res.send('running');
 });
+
+app.use('/children', endpoints.childrenEP);
 
 app.listen(config.port, () => {
     console.log(`listening to http://locaholst:${config.port}`);

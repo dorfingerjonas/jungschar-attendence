990674f0bf9b1fb3fca5265e6e8462a6fbccc421
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-30 21:03:58 +0200

    server: implement post endpoint

--- server/services/ChildrenEndpoint.js
+++ server/services/ChildrenEndpoint.js
@@ -19,4 +19,23 @@ app.get('/:id', async (req, res) => {
     res.send(await repo.findById(req.params?.id));
 });
 
+app.post('/', async (req, res) => {
+    const child = req.body;
+
+    child.id = parseInt(child.id);
+    child.groupId = parseInt(child.groupId);
+
+    if (
+        !child.name
+        || !child.groupId
+        || isNaN(child.id)
+        || isNaN(child.groupId)
+    ) {
+        res.sendStatus(400);
+    } else {
+        res.contentType('application/json');
+        res.send(await repo.add(req.body));
+    }
+});
+
 module.exports = app;

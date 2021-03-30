247f924071dfe504ffaedc47a95978fc0d2083d6
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-30 21:03:29 +0200

    implement get endpoints

--- server/services/ChildrenEndpoint.js
+++ server/services/ChildrenEndpoint.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const ChildrenRepository = require('../controller/ChildrenRepository');
+
+const repo = new ChildrenRepository();
+
+app.get('/', async (req, res) => {
     res.contentType('application/json');
-    res.send('children endpoint is running');
+    res.send(await repo.getAll());
+});
+
+app.get('/:id', async (req, res) => {
+    res.contentType('application/json');
+    res.send(await repo.findById(req.params?.id));
 });
 
 module.exports = app;

93c8156a42aa2cf534fd3600082c7e6faf988f0e
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-05 14:56:56 +0200

    add empty route to app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GroupsListComponent} from './components/groups-list/groups-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GroupsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
